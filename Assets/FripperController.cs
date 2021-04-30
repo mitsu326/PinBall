6d783b55f54bd1aa71f7c2d082683dac3ba5e4d4
mitsu326 <cook_ttt@outlook.jp>
2021-05-01 00:39:04 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キーを押したとき左フリッパーを動かす
+        if (((Input.GetKeyDown(KeyCode.LeftArrow)) ||(Input.GetKeyDown(KeyCode.A))||(Input.GetKeyDown(KeyCode.DownArrow))||(Input.GetKeyDown(KeyCode.S)))&&tag== "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーを押したとき右フリッパーを動かす
+        if (((Input.GetKeyDown(KeyCode.RightArrow))||(Input.GetKeyDown(KeyCode.D))||(Input.GetKeyDown(KeyCode.DownArrow)) ||(Input.GetKeyDown(KeyCode.S))) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キーが離されたときフリッパーを元に戻す
+        if (((Input.GetKeyUp(KeyCode.LeftArrow))||(Input.GetKeyUp(KeyCode.A))||(Input.GetKeyUp(KeyCode.DownArrow)) ||(Input.GetKeyUp(KeyCode.S))) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (((Input.GetKeyUp(KeyCode.RightArrow))||(Input.GetKeyUp(KeyCode.D))||(Input.GetKeyUp(KeyCode.DownArrow)) ||(Input.GetKeyUp(KeyCode.S))) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/ScoreRecorder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreRecorder : MonoBehaviour
+{
+    private GameObject scoreText;
+    private int score=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreRecord");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = "Score:" + this.score;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 5;
+        }else if ((other.gameObject.tag == "LargeStarTag") || (other.gameObject.tag == "SmallCloudTag"))
+        {
+            this.score += 10;
+        }else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += 20;
+        }
+    }
+}
